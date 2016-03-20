bf5db14a3de47075af2cc867bedaf2ffd8d11feb
Tim Crockett <tim.crockett@gmail.com>
2016-03-20 14:00:21 -0600

    Updated bdaypulls with additional images

--- bdaypulls.sh
+++ bdaypulls.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-images="hello-world seqvence/static-site alpine manomarks/worker mhart/alpine/node python:2.7-alpine redis:alpine postgres:9.4"
+images="hello-world seqvence/static-site alpine manomarks/worker mhart/alpine/node python:2.7-alpine redis:alpine postgres:9.4 busybox java:openjdk-8-jdk-alpine"
 
 for image in $images; do
 	docker pull $image
